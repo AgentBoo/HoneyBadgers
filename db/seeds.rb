b5b110b41fd8805c6e666ce7b6119ee4dd8d301c
ashwinder bhinder <ashwinderbhinder24@gmail.com>
2017-10-03 12:14:41 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'IronGloryInventory.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+        @badge = row.to_hash
+#   puts @available = @badge['Available'].gsub(/\:|\d/,'')
+#    puts @available = @badge['Available'].split(/\:|\D/)
+
+
+    
+    
+  t = Badge.new
+  t.product = row['Product']
+  t.sku = row['SKU']
+  t.price = row['Price']
+  t.size = row['Available'].gsub(/\:|\d/, '')
+  t.quantity = row['Available'].gsub(/\:|\D/, '')
+  t.year = row['Year']
+  t.description = row['Description']
+  t.category = row['Category']
+  t.save
+  
+#    
+#  badge = { 1 => { featured: "true"}, 2 => {featured: "true"}}
+#  Badge.update(badge.keys, badge.values)
+    
+end
