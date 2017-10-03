691dd69c21037caadcb796e095cb02306338f1c0
AgentBoo <zuzi.han@gmail.com>
2017-10-03 14:20:42 -0400

    gem rack-cors added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     collection do
       get 'all'
     end
-  end 
+  end
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
