c853d2d27de147b4c2904e4fbba3068487f26156
AgentBoo <zuzi.han@gmail.com>
2017-10-05 05:04:54 -0400

    generated refile initializer using rails g and modified application schema in application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module Honeybadger
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Application must have sql as schema format to properly dump OID type.
+    config.active_record.schema_format = :sql
   end
 end
